c9611e1a34df667b5cb9a63973876e618ca009fa
Ubuntu <ubuntu@ip-172-31-24-172.ec2.internal>
2019-08-29 09:46:19 +0000

    added bootstrap testpage,static and vendor folder

--- walkie_project/urls.py
+++ walkie_project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
 ]
