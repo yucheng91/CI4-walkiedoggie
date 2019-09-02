021d9044d3ef13e2a87c2e838df70e62cfac43f1
Ubuntu <ubuntu@ip-172-31-24-172.ec2.internal>
2019-09-02 04:13:54 +0000

    added edit button & superuser only view

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import walkingdog, coverdog, create_coverdog
+from .views import walkingdog, coverdog, create_coverdog, edit_coverdog
 
 urlpatterns = [
     path('walkingdog/', walkingdog, name='walkingdog'),
     path('coverdog/', coverdog, name='coverdog'),
-    path('coverdog/create/',create_coverdog,name="create_coverdog")
+    path('coverdog/create/',create_coverdog,name="create_coverdog"),
+    path('coverdog/edit/',edit_coverdog,name="edit_coverdog")
 ]
--- post/views.py
+++ post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
@@ -21,8 +21,25 @@ def create_coverdog(request):
             new_blog_post.author = request.user
             new_blog_post.save()
             return redirect(reverse('coverdog'))
+        pass
     else:
         form = PostForm()
         return render(request, 'create_coverdog.html', {
             'form': form
         })
+        
+def edit_coverdog(request, id):
+    post = get_object_or_404(Post, pk=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST,instance=post)
+        if form.is_valid():
+            new_blog_post = form.save(commit=False)
+            new_blog_post.author = request.user
+            new_blog_post.save()
+            return redirect('coverdog')
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'create_coverdog.html',{
+            'form':form
+        })
+        
