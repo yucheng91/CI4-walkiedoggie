d60b3de3d595acb0ac88ee86de497665b5b878ea
Ubuntu <ubuntu@ip-172-31-24-172.ec2.internal>
2019-09-05 02:47:19 +0000

    remove unnecessary models.py, fixed empty donation amount routing

--- donate/views.py
+++ donate/views.py
@@ -7,26 +7,29 @@ def donate(request):
     return render(request, 'donate.html')
     
 def charge(request):
-    amount = int( request.GET['amount']) * 100
-    stripe.api_key = settings.STRIPE_SECRET_KEY
-
-    session = stripe.checkout.Session.create(payment_method_types=['card'],
-        line_items=[
-            {
-                'name': 'Donation',
-                'description':'A simple donation',
-                'amount':amount ,
-                'currency':'sgd',
-                'quantity':1
-            }
-        ],
-        success_url= request.build_absolute_uri(reverse('success')),
-        cancel_url= request.build_absolute_uri(reverse('cancel')),
-    )
-    return render(request, 'charge.html', {
-        'CHECKOUT_SESSION_ID': session.id,
-        'publishable_key': settings.STRIPE_PUBLISHABLE_KEY
-    })
+    if request.GET['amount'] == "":
+        return render(request,"cancel.html")
+    else:
+        amount = int(request.GET['amount']) * 100
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+    
+        session = stripe.checkout.Session.create(payment_method_types=['card'],
+            line_items=[
+                {
+                    'name': 'Donation',
+                    'description':'A simple donation',
+                    'amount':amount ,
+                    'currency':'sgd',
+                    'quantity':1
+                }
+            ],
+            success_url= request.build_absolute_uri(reverse('success')),
+            cancel_url= request.build_absolute_uri(reverse('cancel')),
+        )
+        return render(request, 'charge.html', {
+            'CHECKOUT_SESSION_ID': session.id,
+            'publishable_key': settings.STRIPE_PUBLISHABLE_KEY
+        })
     
    
     
